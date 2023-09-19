a8d65517ec0408d672c1e96133ea8a71cda15c12
devanfer02 <ubdevanferrel04@student.ub.ac.id>
2023-09-19 23:08:35 +0700

    add: figlet lol

--- src/app.ts
+++ src/app.ts
@@ -4,49 +4,57 @@ import helmet from 'helmet';
 import session from 'express-session';
 import SequelizeStore from 'connect-session-sequelize';
 import cookieParser from 'cookie-parser';
+import figlet from 'figlet';
 
 import env from './config/env';
 import db from './config/db';
 import apiKeyInterceptor from './middlewares/ApiKeyInterceptor';
+import { deserializeUser } from './middlewares/Auth';   
 
 import PostRoutes from './routes/PostRoutes';
 import AuthRoutes from './routes/AuthRoutes';
 import UserRoutes from './routes/UserRoutes';
 import FollowRoutes from './routes/FollowRoutes';
 import CommentRoutes from './routes/CommentRoutes'
-import { deserializeUser } from './middlewares/Auth';
 
-db.sync().then(() => {
-    console.log("connection to database established!")
-});
+const init = async () => {
+    await db.sync();
+    figlet("EXPRESS SERVER", (err, data) => {
+        console.log("=".repeat(120));
+        console.log(data);
+        console.log("=".repeat(120));
+        console.log("CONNECTION TO DATABASE ESTABLISHED!");
+        console.log(`SERVER IS RUNNING AT http://localhost:${env.serverPort}`);
+    });
+    
+    const SessionStore   = SequelizeStore(session.Store);
+    const sessionStorage = new SessionStore({
+        db
+    });
+    const app = express();
+    
+    app.use(session({
+        secret: env.sessionKey,
+        resave: false,
+        saveUninitialized: true,
+        store: sessionStorage,
+        cookie: {
+            secure: 'auto',
+        }
+    }));
+    app.use(helmet());
+    app.use(cors());
+    app.use(express.json());
+    app.use(cookieParser());
+    app.use(apiKeyInterceptor.validate.bind(apiKeyInterceptor));
+    app.use(AuthRoutes);
+    app.use(deserializeUser);
+    app.use(PostRoutes);
+    app.use(UserRoutes);
+    app.use(FollowRoutes);
+    app.use(CommentRoutes);
+    
+    app.listen(env.serverPort, () => {});
+}
 
-const SessionStore   = SequelizeStore(session.Store);
-const sessionStorage = new SessionStore({
-    db
-});
-const app = express();
-
-app.use(session({
-    secret: env.sessionKey,
-    resave: false,
-    saveUninitialized: true,
-    store: sessionStorage,
-    cookie: {
-        secure: 'auto',
-    }
-}));
-app.use(helmet());
-app.use(cors());
-app.use(express.json());
-app.use(cookieParser());
-app.use(apiKeyInterceptor.validate.bind(apiKeyInterceptor));
-app.use(AuthRoutes);
-app.use(deserializeUser);
-app.use(PostRoutes);
-app.use(UserRoutes);
-app.use(FollowRoutes);
-app.use(CommentRoutes);
-
-app.listen( env.serverPort, () => {
-    console.log(`server is running at http://localhost:${env.serverPort}`);
-})
+init();
